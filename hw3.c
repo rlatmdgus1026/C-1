4aaacc5593d41e66707cbf4fe1813843ec109c23
rlatmdgus1026 <ksh051026@gmail.com>
2024-10-02 15:10:11 +0900

    hw3

---
+++ hw3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5; // 피라미드의 줄 수
+
+    for (i = 1; i <= rows; i++) {
+        
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
